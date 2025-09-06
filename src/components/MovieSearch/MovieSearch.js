a11ac8d5e02e44e68e2aa00cc313c8cea68be929
slim <slimabida21@gmail.com>
2025-09-06 15:54:06 +0200

    Dynamisation de l'input de recherche de film et mise en place du react-bounce-input et stylisation des resultats de recherche test Ok

--- src/components/MovieSearch/MovieSearch.js
+++ src/components/MovieSearch/MovieSearch.js
@@ -1,21 +1,32 @@
-"use client"
+"use client";
 import { useState } from "react";
+import { DebounceInput } from "react-debounce-input";
+import MovieSearchResults from "./MovieSearchResults/MovieSearchResults";
+import styles from './MovieSearch.module.scss'
 
 function MovieSearch() {
-  // State // 
-  const [movieResults, setMovieResults] = useState([])
-  // Fonction // 
-  async function updateMovieSerach (query) {
-    const response = await fetch(`/api/movies/search?query=${query}`)
-    const { results } = await response.json(); 
-    console.log(results); 
-    setMovieResults([]); 
+  // State //
+  const [movieResults, setMovieResults] = useState([]);
+  const [hasFocus, setHasFocus ] = useState(false); 
+  // Fonction //
+  async function updateMovieSerach(query) {
+    const response = await fetch(`/api/movies/search?query=${query}`);
+    const { results } = await response.json();
+    setMovieResults(results.filter((movie) => movie.backdrop_path));
   }
   return (
-    <div>
-       <input type="text" placeholder="Rechercher un titre ..." onChange={ e => updateMovieSerach(e.target.value)}/>
+    <div className={styles.searchContainer}>
+      <DebounceInput
+        minLength={2}
+        debounceTimeout={500}
+        onChange={(e) => updateMovieSerach(e.target.value)}
+        placeholder="Rechercher un titre ..."
+        onBlur={() => setHasFocus(false)}
+        onFocus={() => setHasFocus(true)}
+      />
+      {movieResults.length > 0 && hasFocus && <MovieSearchResults movieResults={movieResults}/>}
     </div>
-  )
+  );
 }
 
-export default MovieSearch
+export default MovieSearch;
